463c4781a5a9a0c72d0b2bbcd98348e2efd6b85d
purvak2878 <purvakulkarni456@gmail.com>
2026-06-09 14:44:44 +0530

    Added List Interface and Polymorphism examples

---
+++ src/ListDemo.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListDemo {
+
+    public static void main(String[] args) {
+
+        List<String> animals = new ArrayList<>();
+
+        animals.add("Lion");
+        animals.add("Tiger");
+        animals.add("Elephant");
+        animals.add("Monkey");
+        animals.add("Zebra");
+
+        System.out.println("Initial Animal List:");
+        System.out.println(animals);
+
+        animals.add(2, "Giraffe");
+        animals.set(3, "Panda");
+        animals.remove("Monkey");
+
+        System.out.println("\nContains Tiger? " +
+                animals.contains("Tiger"));
+
+        System.out.println("Animal at index 1: " +
+                animals.get(1));
+
+        System.out.println("Total Animals: " +
+                animals.size());
+
+        System.out.println("\nFinal Animal List:");
+
+        for (String animal : animals) {
+            System.out.println(animal);
+        }
+    }
+}
---
+++ src/PolymorphismDemo.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+class polymorphismDemo{
+
+    public static void manageAnimals(List<String> animals) {
+
+        animals.add("Lion");
+        animals.add("Tiger");
+        animals.add("Elephant");
+        animals.add("Giraffe");
+        animals.add("Zebra");
+
+        System.out.println("\nList Type: "
+                + animals.getClass().getSimpleName());
+
+        System.out.println("Animals in Zoo:");
+
+        for (String animal : animals) {
+            System.out.println(animal);
+        }
+
+        System.out.println("Total Animals: "
+                + animals.size());
+
+        System.out.println("Contains Tiger? "
+                + animals.contains("Tiger"));
+    }
+
+    public static void main(String[] args) {
+
+        List<String> arrayListAnimals = new ArrayList<>();
+        List<String> linkedListAnimals = new LinkedList<>();
+
+        manageAnimals(arrayListAnimals);
+        manageAnimals(linkedListAnimals);
+    }
+}
